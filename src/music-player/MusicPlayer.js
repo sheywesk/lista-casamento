081cb1f86ac3dcb5070ce2709036d2512cbf8750
sheywesk medeiros <sheywesk@hotmail.com>
2024-08-07 21:18:43 -0300

    adicionando musica

---
+++ src/music-player/MusicPlayer.js
@@ -0,0 +1,68 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { FaPlay, FaPause } from 'react-icons/fa';
+import './MusicPlayer.css';
+import praVoce from "../assets/music/marcelo_jeneci.mp3"
+
+const MusicPlayer = () => {
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [volume, setVolume] = useState(1);
+    const [autoplayBlocked, setAutoplayBlocked] = useState(false);
+
+    useEffect(() => {
+        const playAudio = async () => {
+            try {
+                await audioRef.current.play();
+                setIsPlaying(true);
+            } catch (error) {
+                setAutoplayBlocked(true);
+                console.log('Autoplay was prevented:', error);
+            }
+        };
+
+        playAudio();
+    }, []);
+
+    const startAudio = () => {
+        audioRef.current.play().then(() => {
+            setIsPlaying(true);
+            setAutoplayBlocked(false);
+        }).catch(error => {
+            console.log('Failed to play audio:', error);
+        });
+    };
+
+    const pauseAudio = () => {
+        audioRef.current.pause();
+        setIsPlaying(false);
+    };
+
+    const handleVolumeChange = (e) => {
+        const newVolume = e.target.value;
+        setVolume(newVolume);
+        audioRef.current.volume = newVolume;
+    };
+
+    return (
+        <div className="music-player">
+            <audio ref={audioRef} src={praVoce} loop />
+            {autoplayBlocked && (
+                <p>Autoplay was blocked. Click the play button to start music.</p>
+            )}
+            <div className="fixed-button">
+                {!isPlaying ? (
+                    <button onClick={startAudio} className="play-button">
+                        <FaPlay />
+                    </button>
+                ) : (
+                    <button onClick={pauseAudio} className="pause-button">
+                        <FaPause />
+                    </button>
+                )}
+            </div>
+        
+        </div>
+    );
+};
+
+export default MusicPlayer;
