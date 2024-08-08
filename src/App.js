31184c04df32691a10152c35b0631dafe7c92ef7
sheywesk medeiros <sheywesk@hotmail.com>
2024-08-07 21:18:15 -0300

    adicionando msucia

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './header/Header';
 import Items from './body/Items';
 import './App.css';
+import MusicPlayer from './music-player/MusicPlayer';
 
 function App() {
   return (
       <div className="App">
           <Header />
           <Items/>
+          <MusicPlayer/>
       </div>
   );
 }
